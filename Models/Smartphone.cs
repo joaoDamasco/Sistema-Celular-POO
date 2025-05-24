f167b488950fb4719bd93e1f75fa29442eb075b4
João Vitor Damasco Sabriano <122624564+joaoDamasco@users.noreply.github.com>
2025-05-24 11:39:52 -0300

    Update Smartphone.cs

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -8,24 +8,19 @@ namespace DesafioPOO.Models
         private string Modelo { get; set; }
         private string IMEI { get; set; }
         private int Memoria;
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-
-
         public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
             Modelo = modelo;
             IMEI = imei;
             Memoria = memoria;
-
-            // TODO: Passar os parâmetros do construtor para as propriedades
         }
 
         public void Ligar()
         {
             Console.WriteLine("Ligando...");
         }
-
+        
         public void ReceberLigacao()
         {
             Console.WriteLine("Recebendo ligação...");
